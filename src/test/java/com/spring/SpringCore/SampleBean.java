1c0df966e5992945533cf9b9a510e02691825052
test@gmail.com <test@gmail.com>
2020-01-25 09:36:31 +0530

    first commit

---
+++ src/test/java/com/spring/SpringCore/SampleBean.java
@@ -0,0 +1,16 @@
+package com.spring.SpringCore;
+
+public class SampleBean {
+	
+	private String msg;
+	
+	public void setMsg(String msg)
+	{
+		this.msg=msg;
+	}
+public void show()
+{
+	System.out.println(" hello "+msg);
+
+}
+}
---
+++ src/test/java/com/spring/SpringCore/TestClient.java
@@ -0,0 +1,14 @@
+package com.spring.SpringCore;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.*;
+
+public class TestClient {
+
+	public static void main(String [] str)
+	{
+		
+	}
+	
+}
